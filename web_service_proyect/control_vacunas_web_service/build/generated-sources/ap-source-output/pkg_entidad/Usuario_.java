1d2b0f6278c5d47645d4bc48fc9562858bd407a7
ale395 <alexis.fernandez.rc@gmail.com>
2017-08-17 16:41:35 -0400

    Proyecto Web Service
    Proyecto Web Service Usuarios

---
+++ web_service_proyect/control_vacunas_web_service/build/generated-sources/ap-source-output/pkg_entidad/Usuario_.java
@@ -0,0 +1,15 @@
+package pkg_entidad;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2017-08-17T16:04:18")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> correo;
+    public static volatile SingularAttribute<Usuario, Integer> id;
+    public static volatile SingularAttribute<Usuario, String> nombre;
+
+}
